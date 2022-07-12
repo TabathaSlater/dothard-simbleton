de834673774cd798c00216d4284c3dc4c5de2c2c
TabathaSlater <106984127+TabathaSlater@users.noreply.github.com>
2022-07-12 13:38:59 -0400

    Search bar code

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -33,4 +33,95 @@ export const listOfBusiness = () => {
     )
 
     return businessHTML
-}
+};
+//_____________________________________________________
+
+/*
+    Responsibility:
+        Add event listener. Find the object that matches the search query string
+
+    Parameters:
+        (search string) - The string of item being searched for
+
+    Returns:
+        (object) - The first matching object
+*/
+
+document
+    .querySelector("#content")
+        .addEventListener(
+            "keypress",
+            (keyPressEvent) => {
+                const companySearchResultArticle = document.querySelector(".foundBusinesses")
+
+                if (keyPressEvent.charCode === 13) {
+                    /*
+                        When the user presses 'Enter', find the matching business.
+
+
+                        You can use the `.includes()` string method to
+                        see if a smaller string is part of a larger string.
+
+                        Example:
+                            if (business.companyName.includes(keyPressEvent.target.value)) {
+
+                            }
+                    */
+                   let businessArray = businessList();
+
+                    const foundBusiness = businessArray.find(
+                        // Your callback function goes here
+                        (object) => {
+                            if (object.companyName.toLowerCase().includes(keyPressEvent.target.value.toLowerCase()))
+                            return object
+                            
+                        }
+                    )
+
+                    companySearchResultArticle.innerHTML = ` <section class='business'>
+                    <h2 class='business__name'>${foundBusiness.companyName}</h2>
+                        <div class='business__street'>${foundBusiness.addressFullStreet}</div>
+                        <div class'business_state'>${foundBusiness.addressCity}, ${foundBusiness.addressStateCode} ${foundBusiness.addressZipCode}</div>
+                 </section> `
+                }
+        });
+
+//______________________________________________________
+
+document
+    .querySelector("#content")
+        .addEventListener(
+            "keypress",
+            (keyPressEvent) => {
+                const companySearchResultArticle = document.querySelector(".foundBusinesses")
+
+                if (keyPressEvent.charCode === 13) {
+                    /*
+                        When the user presses 'Enter', find the matching agent.
+
+
+                        You can use the `.includes()` string method to
+                        see if a smaller string is part of a larger string.
+
+                        Example:
+                            if (object.purhcasingAgent.(property).includes(keyPressEvent.target.value)) {
+
+                            }
+                    */
+                   let businessArray = businessList();
+
+                    const foundAgent = businessArray.find(
+                        // Your callback function goes here
+                        (object) => {
+                            if (object.purchasingAgent.nameFirst.toLowerCase().includes(keyPressEvent.target.value.toLowerCase()) || object.purchasingAgent.nameLast.toLowerCase().includes(keyPressEvent.target.value.toLowerCase()))
+                            return object
+                            
+                        }
+                    )
+
+                    companySearchResultArticle.innerHTML = `<section class='agent'><h2>${foundAgent.purchasingAgent.nameFirst} ${foundAgent.purchasingAgent.nameLast}</h2> 
+                    <div>${foundAgent.companyName}</div>
+                    <div>${foundAgent.phoneWork}
+                    </section>`
+                }
+        });
